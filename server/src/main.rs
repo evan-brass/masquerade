0578e698bf7c4abfc7845fe4b2bd6a1ce673359f
Evan Brass <brassevan@gmail.com>
2025-04-10 09:05:56 -0700

    tcp get link-local
    Firefox autodiscovery works for now.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,13 @@
 use eyre::Result;
+use rand::random;
 use stun::{Stun, Class, Method, attr::{*, parse::AttrIter as _, integrity::Integrity}};
-use std::{io::{Error, ErrorKind, Read as _, Write as _}, net::{IpAddr, Ipv4Addr}};
+use std::{collections::BTreeMap, io::{Error, ErrorKind, Read as _, Write as _}, net::{IpAddr, Ipv6Addr}};
 use std::net::SocketAddr;
 use std::rc::Rc;
 use mio::event::Source as _;
 use mio::net::{TcpListener, TcpStream, UdpSocket};
 use mio::{Events, Poll, Interest, Token};
+use std::collections::btree_map::Entry;
 
 // Constants used by this server
 const TURN_REALM: &str = "none";
@@ -15,7 +17,12 @@ const TURN_USER: &str = "guest";
 const TURN_KEY: &[u8] = &[
 	0x01, 0x5c, 0x8a, 0x97, 0x3e, 0xa4, 0xb4, 0xa9, 0xc9, 0x45, 0xf6, 0x90, 0x14, 0x2b, 0xf3, 0xad,
 ];
-const BROADCAST: IpAddr = IpAddr::V6(Ipv4Addr::BROADCAST.to_ipv6_mapped());
+// FUCK: Firefox seems to dislike broadcast addresses so none of my favorite options worked:
+// - ::ffff:255.255.255.255 failed
+// - ff02::1 failed
+// So we're stuck with frickin fe80::ffff:ffff:ffff:ffff which is reserved because it is the token for UDP (probably, at least if you're 64bit)
+const BROADCAST: IpAddr = IpAddr::V6(Ipv6Addr::new(0xfe80, 0, 0, 0, 0xffff, 0xffff, 0xffff, 0xffff));
+const LINK_LOCAL_OCTETS: [u8; 16] = Ipv6Addr::new(0xfe80, 0, 0, 0, 0, 0, 0, 0).octets();
 
 fn would_block<T>(res: &Result<T, Error>) -> bool {
 	match res {
@@ -26,16 +33,32 @@ fn would_block<T>(res: &Result<T, Error>) -> bool {
 
 // These are the turn
 struct Turn {
-	addr: SocketAddr,
+	port: u16,
 	stream: TcpStream,
 	partial: Option<(usize, Rc<[u8]>)>,
 	// TODO: Firefox enforces permissions, so we also might need a map from SocketAddr -> u16 (pseudo port).  I wonder if we use a sorted map again... then firefox would see the remote port changing as they receive, but... IDK
 }
+impl Turn {
+	pub fn send(&mut self, frame: &Rc<[u8]>) {
+		if self.partial.is_some() { return }
+
+		let res = self.stream.write(frame);
+		if would_block(&res) { return }
+		match res {
+			Ok(written) if written < frame.len() => self.partial = Some((written, frame.clone())),
+			Err(e) => {
+				println!("{e:?}");
+			}
+			_ => {}
+		}
+	}
+}
+
 struct TurnServer {
 	udp: UdpSocket,
 	tcp: TcpListener,
 	// NOTE: streams must be sorted by Turn.addr, and
-	streams: Vec<Turn>,
+	streams: BTreeMap<u64, Turn>,
 	// TODO: Add DTLS state
 }
 const UDP: usize = usize::MAX;
@@ -44,7 +67,7 @@ impl TurnServer {
 	pub fn new(addr: SocketAddr) -> Result<Self> {
 		let udp = UdpSocket::bind(addr)?;
 		let tcp = TcpListener::bind(addr)?;
-		Ok(Self { udp, tcp, streams: Vec::new() })
+		Ok(Self { udp, tcp, streams: BTreeMap::new() })
 	}
 
 	fn handle_msg(&mut self, sender: SocketAddr, mut msg: Stun<&mut [u8]>) {
@@ -201,10 +224,8 @@ impl TurnServer {
 					msg.append::<XOR_PEER_ADDRESS, SocketAddr>(&match (peer.ip(), peer.port()) {
 						// Mode 1: Preserve sender
 						(BROADCAST, 65535) => sender,
-						// Mode 2: Conceal sender (Useful for firefox which would otherwise drop unknown senders)
-						(BROADCAST, 65534) => SocketAddr::new(BROADCAST, 65534),
-						// Reserved for future modes
-						(BROADCAST, _) => return,
+						// Mode 2: Firefox by-port
+						(BROADCAST, _) => SocketAddr::new(BROADCAST, sender.port()),
 
 						// Non-broadcast
 						_ => sender
@@ -226,173 +247,136 @@ impl TurnServer {
 			_ => return,
 		}
 
-		let response = &msg.buffer[..msg.len()];
+		let frame = Rc::from(&msg.buffer[..msg.len()]);
 
-		// Broadcast the response to all TCP streams (that aren't currently writing partial data)
-		if receiver.ip() == BROADCAST {
-			let buffer: Rc<[u8]> = Rc::from(response);
-			for t in self.streams.iter_mut() {
-				// Don't send back to ourself
-				if t.addr == sender { continue }
-				// Don't send if there's already a message in flight
-				if t.partial.is_some() { continue }
-
-				let res = t.stream.write(response);
-				if would_block(&res) { continue }
-				match res {
-					Ok(written) if written < response.len() => t.partial = Some((written, buffer.clone())),
-					Err(e) => {
-						println!("{e:?}");
-					}
-					_ => {}
+		match (receiver.ip(), receiver.port()) {
+			//
+			(BROADCAST, 65535 | 65534) => {
+				for turn in self.streams.values_mut() {
+					turn.send(&frame);
 				}
 			}
-		}
-		// Unicast to a specific TCP stream
-		else if let Ok(i) = self.streams.binary_search_by(|t| t.addr.cmp(&receiver)) {
-			let turn = &mut self.streams[i];
-			if turn.partial.is_none() {
-				let res = turn.stream.write(response);
-				if would_block(&res) { return }
-				if let Ok(written) = res {
-					if written < response.len() {
-						turn.partial = Some((0, Rc::from(&response[written..])));
+			// Fucked up hack to support firefox
+			(BROADCAST, port) => {
+				for turn in self.streams.values_mut() {
+					if turn.port == port {
+						turn.send(&frame);
 					}
 				}
 			}
-		}
-		// Unicast to the UDP receiver
-		else {
-			let res = self.udp.send_to(response, receiver);
-			if would_block(&res) { return }
-			if let Err(e) = res {
-				println!("{e:?}");
+			// TCP unicast
+			(IpAddr::V6(dst), _) if dst.is_unicast_link_local() => {
+				let key = u64::from_be_bytes(dst.octets()[8..].try_into().unwrap());
+				let Some(turn) = self.streams.get_mut(&key) else { return };
+				turn.send(&frame);
 			}
-		};
+			// UDP unicast
+			(ip, port) => {
+				let _ = self.udp.send_to(&frame, SocketAddr::new(ip, port));
+			}
+		}
 	}
 
 	pub fn run(mut self) -> Result<std::convert::Infallible> {
 		let mut buffer = [0; 2048];
 		let mut poll = Poll::new()?;
 
-		const EVENT_CAPACITY: usize = 128;
-		let mut events = Events::with_capacity(EVENT_CAPACITY);
+		let mut events = Events::with_capacity(128);
 		self.udp.register(poll.registry(), Token(UDP), Interest::READABLE)?;
 		self.tcp.register(poll.registry(), Token(TCP), Interest::READABLE)?;
-
-		let mut tokens = Vec::new();
-		tokens.try_reserve(EVENT_CAPACITY)?;
 		loop {
-			tokens.clear();
-			// Handle the unique tokens from least to greatest
-			tokens.extend(events.iter().map(|e| e.token().0));
-			tokens.sort();
-			tokens.dedup();
+			for e in events.iter() {
+				match e.token().0 {
+					UDP => loop {
+						let res = self.udp.recv_from(&mut buffer);
+						if would_block(&res) { break }
 
-			let mut removals = 0;
-			let mut removal_index = UDP;
-
-			// Iterate over each unique token
-			for tok in &tokens {
-				match *tok {
-					UDP => {
-						loop {
-							let res = self.udp.recv_from(&mut buffer);
-							if would_block(&res) { break }
-
-							let (len, sender) = res?;
-							let msg = Stun{ buffer: &mut buffer[..] };
-							if msg.len() == len {
-								self.handle_msg(sender, msg);
-							}
-						}
-					}
-					TCP => {
-						loop {
-							let res = self.tcp.accept();
-							if would_block(&res) { break }
-
-							let (mut stream, addr) = res?;
-							stream.set_nodelay(true)?;
-
-							// We should never have two tcp streams with the same remote address.
-							let i = self.streams.binary_search_by(|t| t.addr.cmp(&addr)).unwrap_err();
-
-							// Reserve space for another stream
-							if self.streams.try_reserve(1).is_err() { continue }
-
-							stream.register(poll.registry(), Token(i), Interest::READABLE)?;
-							self.streams.insert(i, Turn{addr, stream, partial: None});
-
-							// Reregister all following streams to fix their Token
-							for j in (i + 1)..self.streams.len() {
-								let turn = &mut self.streams[j];
-								turn.stream.reregister(poll.registry(), Token(j), Interest::READABLE)?;
-							}
-						}
-					}
-					i => {
-						// Adjust the token to match current indexes:
-						let i = i - removals;
-						loop {
-							let turn = &mut self.streams[i];
-
-							if let Some((offset, buffer)) = turn.partial.take() {
-								let res = turn.stream.write(&buffer[offset..]);
-								if !would_block(&res) {
-									let Ok(written) = res else {
-										turn.stream.deregister(poll.registry())?;
-										self.streams.remove(i);
-										if i < removal_index { removal_index = i; }
-										removals += 1;
-										break
-									};
-									turn.partial = Some((offset + written, buffer));
-								}
-							}
-
-							let sender = turn.addr;
-							let res = turn.stream.peek(&mut buffer);
-							if would_block(&res) { break }
-
-							// Handle streams being closed / erroring out
-							let Ok(len) = res else {
-								turn.stream.deregister(poll.registry())?;
-								self.streams.remove(i);
-								removals += 1;
-								if i < removal_index { removal_index = i; }
-								break
-							};
-							// We can't read the msg_len of this packet until we have at least 4 bytes
-							if len < 4 { break }
-							let msg_len = Stun { buffer: &buffer[..] }.len();
-
-							// Close connection if message is too large for our buffer
-							if msg_len > buffer.len() {
-								turn.stream.deregister(poll.registry())?;
-								self.streams.remove(i);
-								if i < removal_index { removal_index = i; }
-								removals += 1;
-								break
-							}
-
-							// If we don't have the full message than wait
-							if msg_len > len { break }
-
-							// Consume the peeked data
-							turn.stream.read_exact(&mut buffer[..msg_len])?;
-
-							let msg = Stun { buffer: &mut buffer[..] };
+						let (len, sender) = res?;
+						let msg = Stun{ buffer: &mut buffer[..] };
+						if msg.len() == len {
 							self.handle_msg(sender, msg);
 						}
 					}
-				}
-			}
+					TCP => loop {
+						let res = self.tcp.accept();
+						if would_block(&res) { break }
 
-			// If we've removed any connections then we need to reregister following connections:
-			if removals > 0 {
-				for i in removal_index..self.streams.len() {
-					self.streams[i].stream.reregister(poll.registry(), Token(i), Interest::READABLE)?;
+						let (mut stream, _) = res?;
+						stream.set_nodelay(true)?;
+
+						let token = random::<u64>() as usize % TCP;
+						let key = token as u64;
+
+						if let Entry::Vacant(slot) = self.streams.entry(key) {
+							poll.registry().register(&mut stream, Token(token), Interest::READABLE | Interest::WRITABLE)?;
+							slot.insert(Turn {
+								stream,
+								port: random(),
+								partial: None
+							});
+						}
+					}
+					tok if e.is_writable() => {
+						let key = tok as u64;
+						let Entry::Occupied(mut entry) = self.streams.entry(key) else { continue };
+						let turn = entry.get_mut();
+						while let Some((offset, buffer)) = turn.partial.take() {
+							let rest = &buffer[offset..];
+							match turn.stream.write(rest) {
+								Ok(written) if written < rest.len() => turn.partial = Some((offset + written, buffer)),
+								Err(e) if e.kind() == ErrorKind::WouldBlock => turn.partial = Some((offset, buffer)),
+								Err(e) => {
+									println!("{e:?}");
+									poll.registry().deregister(&mut turn.stream)?;
+									entry.remove();
+									break;
+								}
+								_ => {}
+							}
+						}
+					}
+					tok => loop {
+						let key = tok as u64;
+						let Entry::Occupied(mut entry) = self.streams.entry(key) else { continue };
+						let turn = entry.get_mut();
+
+						let res = turn.stream.peek(&mut buffer);
+						if would_block(&res) { break }
+
+						// Handle streams being closed / erroring out
+						let Ok(len) = res else {
+							poll.registry().deregister(&mut turn.stream)?;
+							entry.remove();
+							break;
+						};
+
+						// We can't read the msg_len of this packet until we have at least 4 bytes
+						if len < 4 { break }
+						let msg_len = Stun { buffer: &buffer[..] }.len();
+
+						// Close connection if message is too large for our buffer
+						if msg_len > buffer.len() {
+							poll.registry().deregister(&mut turn.stream)?;
+							entry.remove();
+							break
+						}
+
+						// If we don't have the full message than wait
+						if len < msg_len { break }
+
+						// Consume exactly our message:
+						turn.stream.read_exact(&mut buffer[..msg_len])?;
+
+						let msg = Stun { buffer: &mut buffer[..] };
+
+						// Construct sender address from key (u64 -> Link Local ip6)
+						let mut octets = LINK_LOCAL_OCTETS;
+						octets[8..].copy_from_slice(&key.to_be_bytes());
+						let sender = SocketAddr::new(IpAddr::V6(
+							Ipv6Addr::from(octets)
+						), turn.port);
+						self.handle_msg(sender, msg);
+					}
 				}
 			}
 
