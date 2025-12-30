2a9ef8f6f608e387be7de1169f02738159f43ae6
Evan Brass <brassevan@gmail.com>
2025-12-29 17:55:45 -0800

    FUck
    Holy hell that was difficult.  Unlike TCP, SCTP didn't like the local anyip thing I'm doing.  So, instead I've switched to using dstnat (which yes means connection tracking which means potential dos and shit).  But hopefully we only do connection tracking for plaintext UDP encapsulated SCTP packets.
    
    Currently, WebRTC datachannels are working.
    
    I'm very relieved.  When SCTP wasn't responding from the expected ip, I thought it was over for me.

--- cycle.sh
+++ cycle.sh
@@ -1,13 +1,30 @@
+#!/usr/bin/bash
 set -ex
 
 # Enable IP forwarding
 sysctl net.ipv6.conf.all.forwarding=1
 
-# Load the SCTP kernel module, and configure UDP encapsulation on port 4666
-modprobe sctp
-sysctl net.sctp.udp_port=4666
+# SCTP containment
+ip netns add sctp-sux
+ip netns exec sctp-sux ip link set lo up
+ip link add name sctp-end type veth peer name veth0
+ip link set veth0 netns sctp-sux
+ip netns exec sctp-sux ip link set veth0 up
+ip link set sctp-end up
 
-# create our interfaces
+# Load the SCTP kernel module, and configure UDP encapsulation on port 9899
+modprobe sctp
+ip netns exec sctp-sux sysctl net.sctp.udp_port=9899
+
+# Use dstnat inside our sctp namespace
+ip netns exec sctp-sux ip -6 address add fd00:0:0::/48 dev veth0 noprefixroute
+ip netns exec sctp-sux ip -6 route add local fd00:0:0::/48 dev lo
+ip netns exec sctp-sux ip -6 route add default via $(ip -6 addr show dev sctp-end scope link | grep -oP 'fe80::[a-f0-9:]+') dev veth0 onlink
+ip netns exec sctp-sux nft add table inet sctp-dstnat
+ip netns exec sctp-sux nft add chain inet sctp-dstnat prerouting { type nat hook prerouting priority dstnat \; }
+ip netns exec sctp-sux nft add rule inet sctp-dstnat prerouting ip6 daddr fd00:0:0::/48 dnat to fd00:0:0::
+
+# Create our interfaces
 ip tuntap add mode tun dev turn-udp
 ip tuntap add mode tun dev turn-tcp
 ip tuntap add mode tun dev ice-dissolve
@@ -20,8 +37,8 @@ ip link set dtls-proxy up
 
 # Configure routes for our TURN interfaces
 ip -6 route add A000::/3 dev turn-udp
-ip -6 route add 2001:470:e9e0:100::/96 dev turn-udp
-ip -6 route add 2001:470:e9e0:100:2::/79 dev turn-tcp
+ip -6 route add 2001:470:e9e0:100:2:0::/96 dev turn-udp
+ip -6 route add 2001:470:e9e0:100:4::/79 dev turn-tcp
 
 # Configure all TURN traffic to be ice-dissolve'd
 ip -6 rule add iif turn-udp table 1
@@ -29,20 +46,20 @@ ip -6 rule add iif turn-tcp table 1
 ip -6 route add ::/0 dev ice-dissolve table 1
 
 # Configure our DTLS proxy
-# 1. Assign our /48 subnet to lo (the only interface that can have a subnet as an address)
-ip -6 address add fd00:0:0::/48 dev lo noprefixroute
-# 2. Route decrypted input traffic to lo
-ip -6 rule add iif dtls-proxy table 2
-ip -6 route add local fd00:0:0:c4b0::/64 dev lo table 2
-# 3. Route plaintext output traffic to dtls-proxy
-ip -6 rule add oif lo from fd00:0:0:c4b0::/64 table 3
-ip -6 route add ::/0 dev dtls-proxy table 3
-# 4. Route handshake/ciphertext to dtls-proxy
+# 1. Route handshake/ciphertext to dtls-proxy
 ip -6 route add fd00:0:0:c4b0::/64 dev dtls-proxy
 
+# 2. Route decrypted input traffic to sctp-end
+ip -6 rule add iif dtls-proxy table 2
+ip -6 route add fd00:0:0::/48 via fd00:0:0:: dev sctp-end onlink table 2
+
+# 3. Route plaintext output traffic to dtls-proxy
+ip -6 rule add iif sctp-end from fd00:0:0:c4b0::/64 table 3
+ip -6 route add ::/0 dev dtls-proxy table 3
+
 # Startup our programs
-opt/arm64/bin/turnserver-udp -i turn-udp -m "::ffff:0:0/96<->2001:470:e9e0:100::/96,2000::/3<->A000::/3" &
-opt/arm64/bin/turnserver-tcp -i turn-tcp -s "2001:470:e9e0:100:2::/79" &
+opt/arm64/bin/turnserver-udp -i turn-udp -m "::ffff:0:0/96<->2001:470:e9e0:100:2:0::/96,2000::/3<->A000::/3" &
+opt/arm64/bin/turnserver-tcp -i turn-tcp -s "2001:470:e9e0:100:4::/79" &
 opt/arm64/bin/ice-dissolve -i ice-dissolve &
 opt/arm64/bin/dtls-proxy -i dtls-proxy -c cfg/cert.pem &
-opt/arm64/bin/sctp-echo &
+ip netns exec sctp-sux opt/arm64/bin/sctp-echo &
