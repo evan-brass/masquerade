2e937e45c7f456eb2271fa7abb99f13f1acac2c1
Evan Brass <brassevan@gmail.com>
2025-12-30 22:45:53 -0800

    DTLS Cookies IN OPENSSL!
    Setting the callbacks isn't enough.  There's also an option to cause the thing to do the thing.  Who knew.
    
    Still no inline decrypt or anything.

--- src/bin/dtls-proxy.rs
+++ src/bin/dtls-proxy.rs
@@ -1,15 +1,20 @@
 use std::collections::btree_map::Entry;
 use std::collections::{BTreeMap, VecDeque};
 use std::io::{Error, ErrorKind, Read, Write};
+use std::net::SocketAddrV6;
+use std::time::{Duration, Instant};
 use std::{
-	net::SocketAddr,
 	rc::Rc,
 };
 
 use clap::Parser;
 use eyre::Result;
 use masquerade::wire::{Ip6Header, UdpHeader, ip_checksum, ip_proto};
-use openssl::ssl::{Ssl, SslAcceptor, SslFiletype, SslMethod, SslStream};
+use openssl::hash::MessageDigest;
+use openssl::pkey::PKey;
+use openssl::rand::rand_bytes;
+use openssl::sign::Signer;
+use openssl::ssl::{Ssl, SslAcceptor, SslFiletype, SslMethod, SslOptions, SslStream};
 use tappers::{Interface, Tun};
 use tracing_subscriber::EnvFilter;
 use zerocopy::{FromBytes, IntoBytes};
@@ -94,16 +99,61 @@ fn main() -> Result<Never> {
 	acceptor.set_private_key_file(&args.cert, SslFiletype::PEM)?;
 	acceptor.set_certificate_chain_file(&args.cert)?;
 	acceptor.check_private_key()?;
+
+	// Enable DTLS Cookies
+	acceptor.set_options(SslOptions::COOKIE_EXCHANGE);
+	let cookie_info = Ssl::new_ex_index::<SocketAddrV6>()?;
+	let mut cookey = [0; 16];
+	rand_bytes(&mut cookey)?;
+	let startup = Instant::now();
+
+	acceptor.set_cookie_generate_cb(move |ssl, output| {
+		let info = ssl.ex_data(cookie_info).unwrap();
+		let (timestamp, rest) = output.split_first_chunk_mut().unwrap();
+		let (mac, _) = rest.split_first_chunk_mut::<20>().unwrap();
+		*timestamp = startup.elapsed().as_secs().to_be_bytes();
+
+		let cookey = PKey::hmac(&cookey)?;
+		let mut s = Signer::new(MessageDigest::sha1(), &cookey)?;
+		s.update(timestamp)?;
+		s.update(&info.ip().octets())?;
+		s.update(&info.port().to_be_bytes())?;
+		let len = s.sign(mac)?;
+
+		Ok(timestamp.len() + len)
+	});
+	acceptor.set_cookie_verify_cb(move |ssl, input| {
+		let info = ssl.ex_data(cookie_info).unwrap();
+		let Some((timestamp, mac)) = input.split_first_chunk::<8>() else { return false };
+		let then = u64::from_be_bytes(*timestamp);
+		let now = startup.elapsed().as_secs();
+		let Some(how_old) = now.checked_sub(then).map(Duration::from_secs) else { return false };
+		let too_old = Duration::from_mins(5);
+		if how_old > too_old { 
+			eprint!("Too OLD");
+			return false
+		}
+
+		let cookey = PKey::hmac(&cookey).unwrap();
+		let mut v = Signer::new(MessageDigest::sha1(), &cookey).unwrap();
+		v.update(timestamp).unwrap();
+		v.update(&info.ip().octets()).unwrap();
+		v.update(&info.port().to_be_bytes()).unwrap();
+		let mut temp = [0; 20];
+		assert_eq!(v.sign(&mut temp).unwrap(), temp.len(), "HMAC sign wrong length");
+		temp == mac
+	});
+
 	let context = acceptor.build().into_context();
 	
 	//
 	let mut buffer = [0; 65536];
-	let mut streams = BTreeMap::<SocketAddr, SslStream<_>>::new();
+	let mut streams = BTreeMap::<SocketAddrV6, SslStream<_>>::new();
 
 	loop {
 		let Ok(length) = network.recv(&mut buffer) else { continue };
 
-		// TODO: Cleanup old connections using a last encrypt timeout (destination must respond to keep the connection alive: SCTP would keep this up with heartbeats)
+		// TODO: Cleanup old connections using a last encrypt timeout (destination must respond to keep the connection alive: SCTP would keep this up with heartbeats, but actually you could also keep this up by sending INIT requests and getting ABORT responses... so we're screwed.)
 
 		// Packet:
 		let (ip, rest) = Ip6Header::mut_from_prefix(buffer.as_mut_slice()).unwrap();
@@ -120,8 +170,11 @@ fn main() -> Result<Never> {
 		let data_len = udp.length.get() as usize - size_of::<UdpHeader>();
 		let data = &rest[..data_len];
 
+		let dst = SocketAddrV6::new(ip.dst.into(), udp.dst_port.get(), 0, 0);
+		let src = SocketAddrV6::new(ip.src.into(), udp.src_port.get(), 0, 0);
+
 		// Encrypt the UDP payload and re-emit
-		if let Some(stream) = streams.get_mut(&SocketAddr::new(ip.src.into(), udp.src_port.get())) {
+		if let Some(stream) = streams.get_mut(&src) {
 			// TODO: Set the prefix to unmodified ip+udp, then call ssl_write if our handshake state is complete.
 			let Buffers { buffer, .. } = stream.get_mut();
 			buffer.clear();
@@ -134,12 +187,11 @@ fn main() -> Result<Never> {
 
 		// Handshake or decrypt the payload
 		else {
-			let dst = SocketAddr::new(ip.dst.into(), udp.dst_port.get());
-
 			let mut occupied = match streams.entry(dst) {
 				Entry::Occupied(e) => e,
 				Entry::Vacant(e) => {
 					let mut ssl = Ssl::new(&context)?;
+					ssl.set_ex_data(cookie_info, dst);
 					ssl.set_accept_state();
 					let buffers = Buffers {
 						buffer: Vec::new(),
