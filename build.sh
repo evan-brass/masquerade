062d3ebe09de6fc376b739d27371eb7cc7c6c480
Evan Brass <brassevan@gmail.com>
2026-02-05 12:19:22 -0800

    manually install libsrtp2-dev
    libsrtp2-dev:arm64 and libsrtp2-dev:amd64 "conflict" however the overlapping files are identical.  So, I manually downloaded and "installed" them both sot hat we don't need to swap back and forth anymore.

--- build.sh
+++ build.sh
@@ -4,7 +4,5 @@ set -ex
 # I don't know shit about pkg-config, so this is prob wrong.  But I intend to switch dtls impl anyway eventually so 
 export PKG_CONFIG_PATH=
 export PKG_CONFIG_SYSROOT_DIR=
-sudo apt install -y libsrtp2-dev:arm64
 PKG_CONFIG_LIBDIR=/usr/lib/aarch64-linux-gnu/pkgconfig cargo install --root opt/arm64 --path . --target aarch64-unknown-linux-gnu
-sudo apt install -y libsrtp2-dev:amd64
 PKG_CONFIG_LIBDIR=/usr/lib/x86_64-linux-gnu/pkgconfig cargo install --root opt/amd64 --path . --target x86_64-unknown-linux-gnu
